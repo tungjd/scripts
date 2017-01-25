4f8e9f07c7b3d1be622375c2a63647ba169494ab
tungjd <tungjd@gmail.com>
2017-01-25 12:24:27 -0800

    Use HTTPS and Shows Service Status
    Download source is now
    "https://hyperfish.blob.core.windows.net/files/HyperfishAgent.msi"
    
    Output current service information after upgrade completes:  agent
    version, sa name, state

--- UpgradeAgent.ps1
+++ UpgradeAgent.ps1
@@ -4,46 +4,45 @@ function silentupgrade
     $separator = "@"
     $parts = $svcaccount.split($separator)
     $appdatapath = "C:\Users\" + $parts[0] + "\Appdata\Local\Hyperfish"
-    $serviceproperties = Get-WmiObject Win32_Service | where-object {$_.name -eq "HyperfishAgent"} | select StartName,StartMode,State,Status
     $svc = Get-Service HyperfishAgent
-
     #Create Download Folder If Not Exist
     $dlpath = $appdatapath + "\Installer"
-    $dlpathwc = $dlpath + "\*"
     If(!(test-path $dlpath))
         {
         New-Item -ItemType Directory -Force -Path $dlpath        
         }
-
-    #Download latest agent .msi from http://files.hyperfish.com/files/HyperfishAgent.msi
+    #Download latest agent .msi from https://hyperfish.blob.core.windows.net/files/HyperfishAgent.msi
     Start-Sleep -s 2
-    $agentdlurl = "http://files.hyperfish.com/files/HyperfishAgent.msi"
+    $agentdlurl = "https://hyperfish.blob.core.windows.net/files/HyperfishAgent.msi"
     $output = "$dlpath\HyperfishAgent.msi"
     $start_time = Get-Date
-    Write-Verbose -verbose "Downloading latest Hyperfish Agent installer"
+    echo "Downloading latest Hyperfish Agent installer"
     (New-Object System.Net.WebClient).DownloadFile($agentdlurl, $output)
-    Write-Verbose -verbose "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" 
     #Check if download is complete?
         If(!(test-path $output))
         {
         Start-sleep -s 5     
         }
+    echo "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" 
     #Stop Hyperfish Service
-    Write-Verbose -verbose "Stopping Hyperfish Agent Service"
+    echo "Stopping Hyperfish Agent Service"
     Stop-Service -name "HyperfishAgent" -force
     $svc.WaitForStatus('Stopped','00:05:00')
     #Install .msi silently
     Start-Process $output /qn -Wait
-    Write-Verbose -verbose "Hyperfish Agent Upgrade Complete"
+    echo "Hyperfish Agent Upgrade Complete"
     #Start Hyperfish Service
-    Write-Verbose -verbose "Starting Hyperfish Agent Service"
+    echo "Starting Hyperfish Agent Service"
     Start-Service -name "HyperfishAgent"
     $svc.WaitForStatus('Running','00:05:00')
     #Delete .msi
-    Write-Verbose -verbose "Cleaning up..."
+    echo "Cleaning up..."
     Remove-Item $dlpath -Recurse
-    Write-Verbose -verbose "Upgrade complete. Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-    Echo $serviceproperties
+    echo "Upgrade complete. Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+    $hyperfishapp = Get-WmiObject -Class Win32_Product | Where-Object {$_.name -match "Hyperfish"} | select Version
+    $serviceproperties = Get-WmiObject Win32_Service | where-object {$_.name -eq "HyperfishAgent"} | select StartName,StartMode,State,Status
+    $agentinfo = "`r`nInstalled agent: `r`n" + $hyperfishapp + " `r`n`r`nService Info: `r`n" + $serviceproperties
+    echo $agentinfo 
     Start-Sleep -s 10
   }
 
